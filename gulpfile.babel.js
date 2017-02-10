2fe03be791bb3a41d1f84656d755f1af5313cd1d
Lyra <laurene.v@outlook.com>
2017-02-10 12:57:00 +0100

    install sass + gulp task for sass compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,9 @@
-/*var gulp = require('gulp');*/
-
-/*import gulp from 'gulp';*/
-
-
-
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var sass = require("gulp-sass");
 
-gulp.task('es6', () => console.log('I\'m a task written in ES6'));
 gulp.task("es5", function () {
-  return console.log('I\'m a task written in ES5');
+	return console.log('I\'m a task written in ES5');
 });
+gulp.task('es6', () => console.log('I\'m a task written in ES6'));
+gulp.task('sass', () => gulp.src('public/scss/*.scss').pipe(sass()).pipe(gulp.dest('app/css')));
