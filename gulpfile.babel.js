1f86d56a43df3b1862dc09ae2dc4bb1badb60fd5
Lyra <laurene.v@outlook.com>
2017-02-10 12:58:15 +0100

    fix folders names

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,4 +6,4 @@ gulp.task("es5", function () {
 	return console.log('I\'m a task written in ES5');
 });
 gulp.task('es6', () => console.log('I\'m a task written in ES6'));
-gulp.task('sass', () => gulp.src('public/scss/*.scss').pipe(sass()).pipe(gulp.dest('app/css')));
+gulp.task('sass', () => gulp.src('public/scss/*.scss').pipe(sass()).pipe(gulp.dest('public/css')));
